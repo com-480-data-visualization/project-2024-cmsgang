78386517c04610958a184aeef59a35594421cda8
Netzu00 <stefangabriel00@yahoo.com>
2024-05-30 16:19:15 +0200

    Updated template + structured

---
+++ Milestone3/README.md
@@ -0,0 +1,4 @@
+# Useful links
+
+https://color-contrast-checker.deque.com/
+https://www.color-hex.com/color-palette/1835
---
+++ Milestone3/spotifyAPIAuthRequest.js
@@ -0,0 +1,14 @@
+var client_id = '3c70a68b72ef46b8b4a2ed872032c8c8';
+var redirect_uri = 'http://localhost:5050';
+
+var state = generateRandomString(16);
+
+localStorage.setItem(stateKey, state);
+var scope = 'user-read-private user-read-email';
+
+var url = 'https://accounts.spotify.com/authorize';
+url += '?response_type=token';
+url += '&client_id=' + encodeURIComponent(client_id);
+url += '&scope=' + encodeURIComponent(scope);
+url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+url += '&state=' + encodeURIComponent(state);
